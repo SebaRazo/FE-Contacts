e910c0cbe347f9817d3124146d5618c2ed560a78
seba-razo <sebarazo14@gmail.com>
2023-12-06 17:19:01 -0300

    Todo Robusto

---
+++ src/app/interfaces/auth.ts
@@ -0,0 +1,12 @@
+export interface iAuthRequest {
+  email: string;
+  password: string;
+}
+
+export interface iRegisterRequest {
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  userName: string;
+}
--- src/app/pages/signin/signin.module.ts
+++ src/app/pages/signin/signin.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SigninRoutingModule } from './signin-routing.module';
-
+import { FormsModule } from '@angular/forms';
+import { SigninComponent } from './signin.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SigninRoutingModule
-  ]
+  declarations: [SigninComponent],
+  imports: [CommonModule, SigninRoutingModule, FormsModule],
 })
-export class SigninModule { }
+export class SigninModule {}
